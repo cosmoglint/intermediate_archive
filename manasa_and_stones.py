0dd24021807512d5dbc84796f6d58b9c14c5336b
thedoodler <ramprakashsmail@gmail.com>
2020-10-25 07:48:13 +0530

    added manasa and stones

---
+++ manasa_and_stones.py
@@ -0,0 +1,1 @@
+# Manasa is out on a hike with friends. She finds a trail of stones with numbers on them. She starts following the trail and notices that any two consecutive stones' numbers differ by one of two values. Legend has it that there is a treasure trove at the end of the trail. If Manasa can guess the value of the last stone, the treasure will be hers.
