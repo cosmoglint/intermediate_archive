36ff8ae3d5cb2a251135d26aa5c89156caa1ea12
thedoodler <ramprakashsmail@gmail.com>
2020-12-09 18:30:11 +0530

    added hanoi printer

--- recursive/towers_of_hanoi.py
+++ recursive/towers_of_hanoi.py
@@ -10,4 +10,28 @@ def hanoi(n,start,end):
     else:
         return 1 + hanoi(n-1,start,other) + hanoi(n-1,other,end)
 
+
+
+# print steps
+
+def hanoimove(n,start,end):
+    if (n == 1):
+        print(start,"to",end)
+        # return 1
+    else:
+        other = 6-(start+end)
+        hanoimove(n-1,start,other)
+        print(start,"to",end)
+        hanoimove(n-1,other,end)
+
+def hanoiPrint(n,f,h,t):
+    if (n==0):
+        pass
+    else:
+        hanoiPrint(n-1,f,t,h)
+        print(f," to ",t)
+        hanoiPrint(n-1,h,f,t)
+
 print(hanoi(4,1,3))
+hanoimove(4,1,3)
+hanoiPrint(4,"A","B","C")
