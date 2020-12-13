5f78d388f49ec9a53263f78fd1b85d3c032297ec
thedoodler <ramprakashsmail@gmail.com>
2020-12-13 20:59:33 +0530

    START:  numbers and digits problem

---
+++ nums_and_digits.py
@@ -0,0 +1,3 @@
+# given a range, find the count of numbers that have a particular digit n times
+
+# example given a range of 2 to 20, and number one with count n as 1, we have 10,11,12,13,14,15,16,17,18,19, which gives an answer of 10
