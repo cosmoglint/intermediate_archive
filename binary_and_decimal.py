7ca59d8f188674d5a16e2686843ec6b340739095
thedoodler <ramprakashsmail@gmail.com>
2020-12-13 20:55:43 +0530

    START:  binary literal string value problem

---
+++ binary_and_decimal.py
@@ -0,0 +1,3 @@
+# you are given a number n.
+# find the sum of numbers upto where each number is the literal binary string value of the number.
+# for example, the binary value of 2 is "10" converted to int it is 10. so a sum upto 3 will be 1 + 10 + 11 = 22
