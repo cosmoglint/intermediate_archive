8929bd63be51809de850b85aee31b6cf592c96cf
thedoodler <ramprakashsmail@gmail.com>
2020-12-25 18:58:29 +0530

    START:  alice and notebooks

---
+++ alice_and_notebooks.py
@@ -0,0 +1,7 @@
+# alice can produce a particular number of notebooks per day. given a range of days that each customer will wait, find out how many notebooks she needs to make per day to satisfy each customer
+# eg
+# 2 2 1 - customer a
+# 2 3 3 - customer b
+# 2 3 1 - customer c
+# a needs one notebook by day 2, b needs three notebooks at either day 2 or day 3, c needs one notebook at either day 2 or day 3. so she needs to make two notebooks per day, starting from day 1
+# day one she has two notes, day two she has four notes. so she sends off customer a. now in day three she makes two notes for a total of 5 notes and she gives b three and c 1
