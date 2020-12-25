274b46636ea7b721e2715a9d8c2087c7f01441b2
thedoodler <ramprakashsmail@gmail.com>
2020-12-25 17:36:22 +0530

    FIX:  logic for multiple occurences and add test file

--- Multiple_occurences.py
+++ Multiple_occurences.py
@@ -2,13 +2,43 @@
 
 # eg: [1,2,5,7,5,5,2,2,1]   ans: 1-8, 2-6, 5-3, 7-0
 
+import functools
+
 n = int(input())
+mydic = {}
+
+def index_summer(x,y):
+    global mydic
+
+    cy = max(mydic[y]) - min(mydic[y])
+    return x + cy
 
 for i in range(n):
     mydic = {}
     length = int(input())
     lst = list(map(int,input().split()))
 
-    for val,i in enumerate(lst):
-        if i not in mydic.keys():
-            mydic[i] = []
+    for j,val in enumerate(lst):
+        if val not in mydic.keys():
+            mydic[val] = [j]
+        else:
+            mydic[val].append(j)
+
+    ans = functools.reduce(index_summer,list(mydic.keys()),0)
+
+    print(ans)
+    print(mydic)
+
+
+# sample test case
+"""
+1
+9
+1 2 5 7 5 5 2 2 1
+"""
+
+"""
+1
+5
+1 2 3 3 2
+"""
---
+++ test.py
@@ -0,0 +1,24 @@
+
+
+# import functools
+#
+# def mult(x):
+#     if x<2:
+#         x = x * 5
+#     return x
+#
+# lst = [1,2,3,4,5]
+#
+#
+# val = list(map(mult,lst))
+# # val = [i*5 for i in lst]
+#
+# print(val)
+
+
+import itertools
+
+
+lst = [1,1,1,2,3,4,4,4,4,5,6,7,7,7]
+
+newl = itertools.groupby()
