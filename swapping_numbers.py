b5033d8948363f089b2c7b4b4aef9421aa452762
thedoodler <ramprakashsmail@gmail.com>
2020-12-25 21:13:24 +0530

    EDIT:  swapping_numbers

--- swapping_numbers.py
+++ swapping_numbers.py
@@ -2,15 +2,22 @@ a = int(input())
 lst = list(map(int,input().split()))
 
 i = 0
+ans = 0
 while (i<len(lst)):
     val = lst[i]
     if (i+1 == val):
         i += 1
         continue
     else:
-        ans += 1 if abs((i+1 - val) == 1) else 0
+        ans += 1 if (abs((i+1 - val)) == 1) else 0
         # ind = lst.index()
         lst[i] = lst[val-1]
         lst[val-1] = val
-print(lst)
-        
+print(ans)
+
+
+# sample test case
+"""
+5
+1 4 2 3 5
+"""
