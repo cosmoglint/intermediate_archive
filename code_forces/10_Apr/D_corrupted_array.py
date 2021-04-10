2c7bb60f9f1d682d8cb349e73ffc806cd01fb662
ramprakash <ramprakashsmail@gmail.com>
2021-04-10 22:45:48 +0530

    ADD:  corrupted array problem

--- code_forces/10_Apr/D_corrupted_array.py
+++ code_forces/10_Apr/D_corrupted_array.py
@@ -2,12 +2,15 @@ t = int(input())
 
 for i in range(t):
     n = int(input())
-    lst = list(map(int,input()))
+    lst = list(map(int,input().split()))
+    ans = -1
     lst.sort()
-    if sum(lst[:n]) == lst[n-2]:
-        ans = " ".join(lst[:n])
-    elif sum(lst[:n]) == lst[n-1]:
-        ans = " ".join(lst[:n])
+    if sum(lst[:n]) == lst[n]:
+        ans = " ".join([str(i) for i in lst[:n]])
+    elif sum(lst[:n]) == lst[n+1]:
+        ans = " ".join([str(i) for i in  lst[:n]])
     else:
-        ans = -1
+        for i in range(n):
+            if lst[n] + sum(lst[:i]) + sum(lst[i+1:n]) == lst[n+1]:
+                ans = " ".join([str(i) for i in  lst[:i] + [lst[n]] + lst[i+1:n]])
     print(ans)
