43e8032c07097e535fca65f2958dc67f2749f264
cosmoglint <ramprakashsmail@gmail.com>
2023-11-11 15:42:37 +0530

    ADD: comments for question

--- 17_count_and_say.exs
+++ 17_count_and_say.exs
@@ -1,3 +1,16 @@
+# https://leetcode.com/problems/count-and-say/submissions/
+
+
+#The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+
+# countAndSay(1) = "1"
+# countAndSay(n) is the way you would "say" the digit string from countAndSay(n-1), which is then converted into a different digit string.
+# To determine how you "say" a digit string, split it into the minimal number of substrings such that each substring contains exactly one unique digit. Then for each substring, say the number of digits, then say the digit. Finally, concatenate every said digit.
+
+# For example, the saying and conversion for digit string "3322251":
+
+
+
 defmodule Solution do
   @spec count_and_say(n :: integer) :: String.t
 
