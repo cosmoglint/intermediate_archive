f63a9a3fe98914ad0c2e1bf671efaa3ab97d4e2a
cosmoglint <ramprakashsmail@gmail.com>
2023-11-26 20:57:18 +0530

    CHANGE: parantheses matching changes

--- 19_parantheses_matching.exs
+++ 19_parantheses_matching.exs
@@ -19,60 +19,50 @@ defmodule Solution do
     Enum.at(stack, -1)
   end
 
-  def traverse_parantheses("", _, count, max, rev) do
+  def traverse_parantheses("", curr_stack, count, max) do
     cond do
-      rev > max ->
-        rev
+      Enum.count(curr_stack) > 0 ->
+        max = if count > max, do: count, else: max
 
-      count > max ->
-        count
-
-      count <= max ->
-        max
+      _ ->
+        nil
     end
   end
 
-  def traverse_parantheses(par_str, curr_stack, count, max, rev) do
+  def traverse_parantheses(par_str, curr_stack, count, max) do
     curr_value = String.at(par_str, 0)
     cut_str = String.slice(par_str, 1..-1)
     top_value = top_stack(curr_stack)
 
     cond do
-      curr_value == "" ->
-        0
-
       curr_value == "(" ->
         cond do
-          top_value == nil ->
-            traverse_parantheses(cut_str, [], 0, max, 0)
-
           top_value == "(" ->
             new_stack = curr_stack ++ ["("]
-            traverse_parantheses(cut_str, curr_stack, count, max, rev)
+            traverse_parantheses(cut_str, new_stack, count, max)
 
           top_value == ")" ->
-            rev = 0
-            traverse_parantheses(cut_str, curr_stack, count, max, rev)
+            traverse_parantheses(cut_str, curr_stack, count, max)
         end
 
       curr_value == ")" ->
         cond do
           top_value == nil ->
             max = if count > max, do: count, else: max
-            traverse_parantheses(cut_str, [], 0, max, 0)
+            traverse_parantheses(cut_str, [], 0, max)
 
           top_value == "(" ->
             new_stack = Enum.slice(curr_stack, 0..-2)
-            traverse_parantheses(cut_str, new_stack, count + 2, max, rev + 2)
+            traverse_parantheses(cut_str, new_stack, count + 2, max)
 
           top_value == ")" ->
             new_stack = curr_stack ++ [")"]
-            traverse_parantheses(cut_str, new_stack, count, max, rev)
+            traverse_parantheses(cut_str, new_stack, count, max)
         end
     end
   end
 
   def longest_valid_parentheses(s) do
-    traverse_parantheses(s, [], 0, 0, 0)
+    traverse_parantheses(s, [], 0, 0)
   end
 end
